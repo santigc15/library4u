a2b7a61c5927a45a028f11840ce58fd8bc9a974b
santigc15 <gomezcacheiro@gmail.com>
2023-04-09 20:58:34 +0200

    actualizaciones

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 LIBRARYU
-
+https://libray4u.000webhostapp.com/
 Subir y descargar archivos pdf. En proceso de desarrollo la gestión de usuarios. Pendiente de desarrollar el aplicativo de visualización y gestion de los archivos.
--- views/profile.php
+++ views/profile.php
@@ -1,4 +1,4 @@
-<div class="profile-buttons-container">
+<div class="profilebuttonscontainer">
     <a href="" id="modifyProfile" ><button class="modify-button">Profile data modify</button></a>
     <a href="" id="modifyPassword" ><button class="change-button">Change password</button></a>
     <a href="" id="deleteProfile" ><button class="delete-button">Delete Profile</button></a>
